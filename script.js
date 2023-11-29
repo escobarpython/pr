ab600d988ee8522e73a45db873f2754795ba54bf
Pedro Escobar | escobarpython <140730070+escobarpython@users.noreply.github.com>
2023-11-29 19:25:40 -0300

    Major Update 2

---
+++ script.js
@@ -0,0 +1,24 @@
+function calcularNota() {
+    // Obter os valores inseridos pelo usuário
+    const p1 = parseFloat(document.getElementById('p1').value);
+    const p2 = parseFloat(document.getElementById('p2').value);
+    const iaf = parseFloat(document.getElementById('iaf').value);
+    const pr = parseFloat(document.getElementById('pr').value);
+  
+    // Verificar se as notas estão dentro dos limites
+    if (isNaN(p1) || isNaN(p2) || isNaN(iaf) || isNaN(pr) ||
+        p1 < 0 || p1 > 3 ||
+        p2 < 0 || p2 > 2 ||
+        iaf < 0 || iaf > 2 ||
+        pr < 0 || pr > 7) {
+      document.getElementById('resultado').innerText = 'Por favor, insira corretamente as informações. Provavelmente você recebeu esta mensagem pois os dados inseridos não estão de acordo com o maximo proposto. Caso esteja errado, comunique com um print o criador do site (@escobarpython).';
+      return;
+    }
+  
+    // Calcular a nota final considerando as regras
+    const notaFinal = (p1 + p2 + iaf + pr) / 2;
+  
+    // Exibir o resultado
+    document.getElementById('resultado').innerText = `Sua nota final pós PR é: ${notaFinal.toFixed(2)}`;
+  }
+  
